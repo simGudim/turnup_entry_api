8b587685b8b23cda64d6fdb7a39cdcf02c750ff2
Simon Gudim <simong@paradox.com>
2021-09-24 17:58:40 -0400

    learning the application

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def index():
-    return {
-        "data" : {"name" : "Simon"}
-    }
+def start():
+    return "nothiong"
 
 @app.get("/about")
-def about():
-    return {"data" : {"about page"}}
+def about(limit: bool = True, sort: Optional[str] = None):
+    return {"data" : {f"about page : {limit}"}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
 
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, port = 9000, host = "127.0.0.1")
+@app.post("/blog")
+def blog(request: Blog):
+    return request
+
+
+@app.get("/{id}")
+def index(id: int):
+    return {
+        "data" : {"id" : id}
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = "127.0.0.1", port = "9000")
